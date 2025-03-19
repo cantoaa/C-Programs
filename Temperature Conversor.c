d00ddcb726b12c78c10083c034cc823c01aefc26
cantoaa <145704873+cantoaa@users.noreply.github.com>
2025-03-19 11:06:40 -0300

    Add files via upload
    This is a program that convert Celsius to Fahrenheit or Fahrenheit to Celsius.

---
+++ Temperature Conversor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float temp;
+    char type;
+
+    printf("Temperature: ");
+    scanf("%f", temp);
+    printf("\n F(Fahrenheit) or C(Celsius): ");
+    scanf("%c", type);
+    if(type == 'F'){
+        printf("\n In Celsius: %f", (temp - 32) * 5 / 9.0);
+    }
+    else if(type == 'C'){
+        printf("\n In Fahrenheit: %f", ((temp / 5.0)    * 9 + 32));
+    }
+
+    return 0;
+}
